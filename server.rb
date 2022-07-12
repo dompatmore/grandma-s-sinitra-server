108de3ed9c7a017d4fc5b4dd97c5cf5d2753008d
Dom Patmore <dom@pilifter.com>
2022-07-12 15:14:56 +0100

    uploaded with pg info

--- server.rb
+++ server.rb
@@ -1,12 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pg'
 
+#DO NOT DO THIS IN YOUR PROJECT!
+connection = PG::Connection.open(
+    :host =>'35u4rftguyihjopoiufghyujkik',
+     :port => 5432,
+     :dbname => 'trdckygvjuyhg7yv,hb.jkkpigv',
+      :user => '6tr7fgcvh8y7yf6tgcvhbhu8hyg',
+       :password => 'r6ft5t cgvmr65fu7ghcv nkfrutyvglgjhbmn')
 
-# get '/' do
-#     'Hi, sassy grandma!'
-# end
-
-get '/:name?' do |name|
-    erb :index, :locals => {:name=>name} 
+get '/' do
+    "Hey there!"
 end
 
+get '/greeting/:name?' do |name|
+    erb :index, :locals => {:name => name}
+end
+
+get '/pets' do
+    number_of_pets = connection.exec_params("select name from pets").values[1]
+    number_of_pets
+end
